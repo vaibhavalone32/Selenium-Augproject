3bca5546594fc6004041d075f9d4961531a17461
vaibhavalone32 <vaibhavalone32@gmail.com>
2022-03-31 14:41:03 +0530

    new method added

--- Git Man/src/project1/Hero.java
+++ Git Man/src/project1/Hero.java
@@ -6,14 +6,23 @@ public class Hero {
 
 		System.out.println("jo mai bolta hu o me karta hu");
 	}
-	
-	public void m1(){
-	
-	
-	
-	
-		
+
+	public void m1() {
+
 		System.out.println("m1 method");
-	
+
 	}
+
+	public void m2() {
+
+		System.out.println("===Heropanti===");
+
+	}
+
+	public void m3() {
+
+		System.out.println("RRR_Movie");
+
+	}
+
 }
